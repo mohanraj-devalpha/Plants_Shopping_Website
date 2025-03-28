5671f9eb186721c102250f55c207f64025ea889a
mohanraj-devalpha <mohanraj.s1039@ptuniv.edu.in>
2025-03-28 17:54:47 +0530

    Static UI Created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "./components/Home.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const Counter = ({ end = 100, duration = 3000 }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let start = 0;
+    const increment = Math.ceil(end / (duration / 16)); // Approximate frame duration
+    const timer = setInterval(() => {
+      start += increment;
+      if (start >= end) {
+        start = end;
+        clearInterval(timer);
+
+      }
+
+      setCount(start);
+    }, 16); // Roughly 60 FPS
+
+    return () => clearInterval(timer);
+  }, [end, duration]);
+
+  console.log(count)
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <span className=" font-bold text-4xl">
+        {count}+
+      </span>
+      <p className="">Lorem ipsum consectetur</p>
+    </div>
+  );
+};
+
+export default Counter;
