acee7dbf3c585a640be497b950615351ec4effe9
mohanraj-devalpha <mohanraj.s1039@ptuniv.edu.in>
2025-03-29 14:11:47 +0530

    padding for th whole page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,15 @@ import Counter from "./Counter.jsx"
 
 const Home = () => {
   return (
+<<<<<<< HEAD
     <div className='flex flex-col space-y-10 border px-10 py-10 w-full '>
         <div className='grid grid-cols-2 justify-between'>
           <div className='bg-[#F2F2F2] rounded-br-[300px] space-y-15 px-20 py-30'>
+=======
+    <div className='flex flex-col space-y-10 border w-full  px-4 md:px-10 lg:px-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 items-center relative justify-between'>
+          <div className='bg-[rgb(242,242,242)]  rounded-br-[300px] space-y-15 px-20 py-30'>
+>>>>>>> d12fc7c (Padding changes)
           <div className='space-y-12'>
             <div className='text-6xl space-y-3 font-bold'>
               <h1 className=''>Bring  <span className='text-[#437223]'>GREEN</span> to</h1>
@@ -23,11 +29,18 @@ const Home = () => {
             <Counter end={123} duration={1000} />
             <Counter end={126} duration={1000} />
             <Counter end={76} duration={1000} />
+<<<<<<< HEAD
 
           </div>
           <button className='bg-[#437223] text-amber-50 px-18 py-5 text-2xl rounded-full'>Find your plant</button>
           </div>
           <div className='absolute right-60 top-55'>
+=======
+          </div>
+          <button className='bg-[#437223] text-amber-50 px-18 py-5 text-2xl rounded-full'>Find your plant</button>
+          </div>
+          <div className='absolute max-[1300px]:hidden right-55 top-55'>
+>>>>>>> d12fc7c (Padding changes)
             <img src={Pot} alt="" />
           </div>
           <div className='absolute right-55 top-45'>
@@ -48,10 +61,14 @@ const Home = () => {
                 <img src={Ellipse_1} alt="" className='px-10 '/>
                 <p className='py-5 content-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus</p>
             </div>
+<<<<<<< HEAD
 
         </div>
         
       
+=======
+        </div>
+>>>>>>> d12fc7c (Padding changes)
     </div>
   )
 }
